dcf95d5f607db5603491cd48a7ada8a7962ff917
andrea-marketing <andrea.marketing.kedge@gmail.com>
2022-06-07 16:26:28 +0200

    stupid pundit

--- app/policies/vehicle_policy.rb
+++ app/policies/vehicle_policy.rb
@@ -1,12 +1,15 @@
 class VehiclePolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
     def resolve
       scope.all
     end
+  end
 
-    def create?
-      return true
-    end
+  def create?
+    return true
   end
 end
