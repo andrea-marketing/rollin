a14fff6e4cdb298b5881e5abb4a2231f93263c1c
Adele Albrespy <a.albrespy@gmail.com>
2022-06-08 11:10:36 +0200

    controller booking

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :vehicles, only: %i[index new create show] do
     resources :bookings, only: %i[create new]
   end
-    resource :bookings, only: %i[index show destroy update]
+    resources :bookings, only: %i[index show destroy update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
