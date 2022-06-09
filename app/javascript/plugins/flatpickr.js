a0178dc69e9b0e57e54a7b9ed961b7ea800e9886
Adele Albrespy <a.albrespy@gmail.com>
2022-06-09 10:47:29 +0200

    methode price

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -4,7 +4,8 @@ import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 const initFlatpickr = () => {
   flatpickr("#range_start", {
     altInput: true,
-    plugins: [new rangePlugin({ input: "#range_end"})]
+    plugins: [new rangePlugin({ input: "#range_end"})],
+    disableMobile: true
   });
 }
 
