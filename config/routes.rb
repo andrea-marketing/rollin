82bec2989e039e05e71e3bab318366de4f323835
Maxim Bernard <maxim.bernard@kedgebs.com>
2022-06-09 14:44:19 +0200

    my-vehicles

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'vehicles#index'
+  get 'dashboard', to: 'pages#dashboard', as: :dashboard
   resources :vehicles do
     resources :bookings, except: [:destroy]
   end
-    resources :bookings, only: [:destroy]
+  resources :bookings, only: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
