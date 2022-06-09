7fdd123d1a933d678988280f3ad28e1b45cfb2b7
Adele Albrespy <a.albrespy@gmail.com>
2022-06-09 15:47:31 +0200

    bla

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
- 
+
   root to: 'pages#home'
-  
+
   get 'home', to: 'pages#home'
   get 'dashboard', to: 'pages#dashboard', as: :dashboard
-  
+
   resources :vehicles do
     resources :bookings, except: [:destroy] do
       member do
@@ -13,6 +13,5 @@ Rails.application.routes.draw do
       end
     end
   end
-
    resources :bookings, only: [:destroy]
 end
