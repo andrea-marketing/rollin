47c672f33ce8d89ae88666477800c72adca79998
andrea-marketing <andrea.marketing.kedge@gmail.com>
2022-06-10 15:40:14 +0200

    seeds

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -4,7 +4,7 @@ class Vehicle < ApplicationRecord
 
   has_one_attached :photo
 
-  CATEGORIES = %w[bike skate rolling-skates ice-skates scooter]
+  CATEGORIES = ["bike", "skate", "rolling skates", "ice skates", "scooter"]
   validates :vehicle_type, inclusion: { in: CATEGORIES }
 
   geocoded_by :address
